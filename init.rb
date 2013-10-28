f80589db82bd52dc09200a07cce9ca7dfd0548b7
Yukinari Toyota <xxseyxx@gmail.com>
2013-10-28 18:51:48 +0900

    initial import.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_pdf_export_backports do
+  name 'Redmine Pdf Export Backports plugin'
+  author 'Yukinari Toyota'
+  description 'This plugin provides backport support of latest pdf export improvoments to redmine 2.2/2.3'
+  version '0.0.1'
+  url 'http://github.com/t-yuki/redmine_pdf_export_backports/'
+  author_url 'http://github.com/t-yuki/'
+end
